42ae9d2809277b405fe26411c613bc67e6c296f8
Sébastien Counen <sebastiencounen@Aureliens-MBP.lan>
2021-01-27 10:13:11 +0100

    Init: Solution Architecture  & NuGet Packages

---
+++ Ingenum.Case.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ingenum.Case.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Ingenum.Case.EntityFramework/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ingenum.Case.EntityFramework
+{
+    public class Class1
+    {
+    }
+}
